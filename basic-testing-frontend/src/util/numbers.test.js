4d86558c3f4f899230cb18ab7973e2377291e668
Jono Jarvis <j.a.k.jarvis@gmail.com>
2023-01-06 13:33:21 +0100

    finished module 5

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,6 +1,6 @@
-import { it, expect, expectTypeOf } from "vitest";
+import { it, expect, expectTypeOf, describe } from "vitest";
 
-import { transformToNumber } from "./numbers";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
 it("should return a number when a numeric string value is passed", () => {
   const input = "1";
@@ -18,4 +18,21 @@ it("should return NaN when a non-numeric string value is passed", () => {
 
   expect(result1).toBeNaN();
   expect(result2).toBeNaN();
+});
+
+describe("cleanNumbers()", () => {
+  it("should return an array of values if an array of string number values is provided", () => {
+    const numberValues = ["1", "2"];
+
+    const cleanedNumbers = cleanNumbers(numberValues);
+
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
+
+  it("should throw an error if an array with at least one empty string is provided", () => {
+    const numberValues = ["", 1];
+    const cleanFn = () => cleanNumbers(numberValues);
+
+    expect(cleanFn).toThrow();
+  });
 });
